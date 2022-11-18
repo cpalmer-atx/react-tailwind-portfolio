c35b3e73692f82bf1f35b98ba5c4d15b98804090
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-18 17:16:36 -0600

    begin styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="relative container mx-auto p-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-3xl font-logo">CP</h1>
+        </div>
+        <ul className="hidden space-x-6 md:flex">
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+        <a href="#">Resume</a>
+
+        <button>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </nav>
   )
 }
 

23d1521b7fec045edd5f24aaabddc02ae8e05c8e
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-18 12:40:38 -0600

    basic components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </>
   )
 }
 

392f033881da17326430f54a96c4baf93ebd078a
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-18 23:03:41 -0600

    added boilerplate code to set up smooth scroll

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,11 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div name='about' className="relative container mx-auto p-6 h-screen w-full max-w-5xl bg-gradient-to-tr from-primary via-primary to-secondary">
+      <div className="flex flex-col p-8 justify-center mx-auto h-full">
+        <h1 className="text-primaryTxt text-4xl">About</h1>
+        <p className="text-secondaryTxt text-2xl pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque culpa asperiores. Distinctio laudantium quae facilis voluptatem mollitia numquam error aperiam corrupti delectus sequi sit saepe voluptate neque, autem corporis!</p>
+      </div>
+    </div>
   )
 }
 

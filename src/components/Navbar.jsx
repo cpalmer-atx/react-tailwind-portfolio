9579ecc86c0aa3c93674c5b4e28353c259c33492
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-18 23:05:24 -0600

    implemented smooth scroll, navbar needs to be fixed to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { FaBars, FaTimes } from "react-icons/fa"
+import { Link } from "react-scroll"
 
 const Navbar = () => {
 
@@ -9,30 +10,36 @@ const Navbar = () => {
   ]
 
   return (
-    <nav className="relative container mx-auto p-6 bg-gradient-to-b from-black via-black to-transparent min-w-full">
+    <nav className="relative container mx-auto p-6  max-w-5xl bg-gradient-to-tr from-primary via-primary to-secondary">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-4xl px-4 font-logo text-transparent bg-clip-text bg-gradient-to-bl from-primaryTxt via-primaryTxt to-secondaryTxt">C.Palmer</h1>
         </div>
         <ul className="hidden space-x-6 text-primaryTxt md:flex">
           {links.map( (link) => (
-            <li key={link} className="cursor-pointer px-2 hover:text-secondaryTxt hover:scale-110">{link}</li>
+            <li key={link} className="cursor-pointer px-2 hover:text-secondaryTxt hover:scale-110">
+              <Link to={link} smooth duration={500}>{link}</Link>
+            </li>
           ))}
         </ul>
         <a href="./" className="hidden md:block p-3 px-6 pt-2 text-secondaryTxt border-2 border-primaryTxt hover:scale-105 hover:border-contrast duration-300 rounded-lg">Résumé</a>
 
         {/* Hamburger button for mobile screens */}
-        <div onClick={ () => setNav(!nav) } className="cursor-pointer text-primaryTxt md:hidden">
+        <div onClick={ () => setNav(!nav) } className="cursor-pointer pr-2 z-20 text-primaryTxt md:hidden">
           { nav ? <FaTimes size={30} /> : <FaBars size={30} /> }
         </div>
 
         {/* Mobile menu */}
         { nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-black text-primaryTxt">
+          <ul className="flex flex-col md:hidden z-10 justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-tr from-primary via-primary to-secondary text-primaryTxt">
+            <h1 className="text-4xl py-3 my-4 px-10 border-b-2 border-primaryTxt font-logo text-transparent bg-clip-text bg-gradient-to-bl from-primaryTxt via-primaryTxt to-secondaryTxt">C.Palmer</h1>
             {links.map( (link) => (
-              <li key={link} onClick={() => setNav(!nav)} className="cursor-pointer py-4 text-2xl hover:text-secondaryTxt hover:scale-110">{link}</li>
+              <li key={link} onClick={() => setNav(!nav)} className="cursor-pointer py-4 text-2xl hover:text-secondaryTxt hover:scale-110">
+                <Link to={link} smooth duration={500}>{link}</Link>
+              </li>
             ))}
-          </ul>
+            <a href="./" className="p-3 px-16 pt-2 mt-8 text-secondaryTxt border-2 border-primaryTxt hover:scale-105 hover:border-contrast duration-300 rounded-lg">Résumé</a>
+          </ul> 
         )}
       </div>
     </nav>

7205c100d965c5ad4748159af36c274aa0b8f8ad
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-20 11:29:27 -0600

    removed gradient from components for now

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 const Experience = () => {
   return (
-    <div name='experience' className="relative container mx-auto p-6 h-screen max-w-full bg-gradient-to-tr from-primary via-primary to-secondary">
+    <div name='experience' className="relative container mx-auto p-6 h-screen max-w-full bg-primary">
       <div className="flex flex-col p-8 justify-center mx-auto h-full max-w-5xl">
         <h1 className="text-primaryTxt text-4xl">Experience</h1>
         <p className="text-secondaryTxt text-2xl pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque culpa asperiores. Distinctio laudantium quae facilis voluptatem mollitia numquam error aperiam corrupti delectus sequi sit saepe voluptate neque, autem corporis!</p>

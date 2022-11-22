280a9dbb2482e047900d8cb34f2ca5c66212b09a
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-22 15:32:42 -0600

    added jobs array to Exp component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,29 @@
 const Experience = () => {
+
+  const jobs = [
+    {
+      employer: 'Katie Kime',
+      title: 'Intern Software Engineer',
+      from: 'June 2021',
+      to: 'Sept 2021',
+      bullets: []
+    },
+    {
+      employer: 'Pensa Systems',
+      title: 'Manufacturing & Test Engineer',
+      from: 'June 2020',
+      to: 'Oct 2020',
+      bullets: []
+    },
+    {
+      employer: 'U.S. Coast Guard',
+      title: 'Marine Science Technician',
+      from: 'March 2005',
+      to: 'July 2015',
+      bullets: []
+    },
+  ]
+  
   return (
     <div name='experience' className="relative container mx-auto p-6 h-screen max-w-full bg-primary">
       <div className="flex flex-col p-8 justify-center mx-auto h-full max-w-5xl">

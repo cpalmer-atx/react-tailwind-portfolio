eea8a4f21c16f12810e39b3676bf108dcf95deb3
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-22 08:16:16 -0600

    home content, rough layout in place

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,31 @@
+import profileImage from '../assets/profileImg.png'
+
 const Home = () => {
+
+  const styles = {
+    home: 'relative container mx-auto p-6 h-screen max-w-full bg-primary',
+    homeContainer: 'flex flex-col p-8 justify-center items-center mx-auto h-full max-w-5xl md:flex-row',
+    homeContent: 'flex flex-col text-secondaryTxt',
+    imageContainer: ' drop-shadow-2xl',
+    profile: 'p-1 mx-auto rounded-tr-3xl rounded-bl-3xl bg-secondaryTxt opacity-75 w-1/2 md:w-full'
+  }
+  const { home, homeContainer, homeContent, imageContainer, profile } = styles
+
   return (
-    <div name='home' className="relative container mx-auto p-6 h-screen max-w-full bg-primary ">
-      <div className="flex flex-col p-8 justify-center mx-auto h-full max-w-5xl">
-        <h1 className="text-primaryTxt text-4xl">Home</h1>
-        <p className="text-secondaryTxt text-2xl pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque culpa asperiores. Distinctio laudantium quae facilis voluptatem mollitia numquam error aperiam corrupti delectus sequi sit saepe voluptate neque, autem corporis!</p>
+    <section name='home' className={home}>
+      <div className={homeContainer}>
+        <div className={homeContent}>
+          <h3 className='text-2xl'>Hi, my name is</h3>
+          <h2 className='text-primaryTxt font-logo text-7xl py-2'>Chad Palmer</h2>
+          <p className='py-4 pr-20'>
+            I'm a software developer bringing you digital solutions from the beautiful Texas hill country.  I specialize in web applications and development utilizing the latest technologies, and I'm always looking for likeminded developers to grow my experience with!
+          </p>
+        </div>
+        <div className={imageContainer}>
+          <img src={profileImage} className={profile} alt='me' />
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 

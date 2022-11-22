9720dba95c1da0c8dd4adfa0f78ced45bc618884
Chad Palmer <chad.n.palmer@gmail.com>
2022-11-22 17:51:16 -0600

    boilerplate text alignment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ const Projects = () => {
   return (
     <div name='projects' className="relative container mx-auto p-6 h-screen max-w-full bg-primary">
       <div className="flex flex-col p-8 justify-center mx-auto h-full max-w-5xl">
-        <h1 className="text-primaryTxt text-4xl">Projects</h1>
-        <p className="text-secondaryTxt text-2xl pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque culpa asperiores. Distinctio laudantium quae facilis voluptatem mollitia numquam error aperiam corrupti delectus sequi sit saepe voluptate neque, autem corporis!</p>
+        <h1 className="text-primaryTxt text-4xl text-center md:text-left">Projects</h1>
+        <p className="text-secondaryTxt text-2xl pt-4 text-center md:text-left">Coming soon!</p>
       </div>
     </div>
   )

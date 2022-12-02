ada3bc0b15ad471b99870d83f9c175279c078721
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-02 11:38:15 -0600

    check for production

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './config/.env' });
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -13,5 +14,14 @@ app.use(cors());
 // Routers
 app.use('/sanityCheck', require('./routes/sanity'));
 app.use('/api/send', require('./routes/nodemailer'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 module.exports = app;

80d8da8372baca9ebfd6505d8fbb022a007cc8d8
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-06 14:44:52 -0600

    jsx cleanup projects component

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -2,6 +2,24 @@ import reactTailwindExpress from '../assets/react-tailwind-express.png'
 
 const Projects = () => {
 
+  const styles = {
+    projectsComponent: 'relative container mx-auto p-6 py-80 lg:py-24 h-screen max-w-full bg-primary',
+    projectsContainer: 'mx-auto p-4 flex flex-col justify-center w-full h-full max-w-5xl',
+    header: 'text-primaryTxt text-4xl text-center md:text-left',
+    description: 'py-4 pr-9 text-secondaryTxt text-center text-sm md:text-left',
+    projectsGrid: 'text-secondaryTxt w-full grid justify-center grid-cols-1 sm:grid-cols-3 gap-8 text-center py-8 px-8 sm:px-0',
+    projectCard: 'shadow-md min-w-min md:max-w-min shadow-gray-600 rounded-lg',
+    projectImg: 'rounded-md duration-200 hover:scale-105',
+    cardBtnContainer: 'flex items-center justify-center',
+    cardButton: 'w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+  }
+  const { projectsComponent, projectsContainer, header, description, projectsGrid, projectCard, projectImg, cardBtnContainer, cardButton } = styles;
+
+  const content = {
+    header: 'Featured Projects',
+    content: "Visit the code for a detailed description of technologies used found in the README.md file.",
+  }
+
   const projects = [
     {
       src: reactTailwindExpress,
@@ -11,26 +29,25 @@ const Projects = () => {
   ]
 
   return (
-    <div name='projects' className='relative container mx-auto p-6 py-80 lg:py-24 h-screen max-w-full bg-primary'>
-      <div className='mx-auto p-4 flex flex-col justify-center w-full h-full max-w-5xl'>
+    <div name='projects' className={projectsComponent}>
+      <div className={projectsContainer}>
         <div>
-          <h1 className='text-primaryTxt text-4xl text-center md:text-left'>Featured Projects</h1>
-          <p className='py-4 pr-9 text-secondaryTxt text-center text-sm md:text-left'>Visit the code for a detailed description of technologies used found in the README.md file.</p>
+          <h1 className={header}>{content.header}</h1>
+          <p className={description}>{content.content}</p>
         </div>
-        <div className='text-secondaryTxt w-full grid justify-center grid-cols-1 sm:grid-cols-3 gap-8 text-center py-8 px-8 sm:px-0'>
-          {/* Reserved for project cards */}
+        <div className={projectsGrid}>
           {projects.map(({ src, demoURL, codeURL }) => (
-            <div key={src} className='shadow-md min-w-min md:max-w-min shadow-gray-600 rounded-lg'>
-              <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
-              <div className='flex items-center justify-center'>
+            <div key={src} className={projectCard}>
+              <img src={src} alt="" className={projectImg} />
+              <div className={cardBtnContainer}>
                 <button 
                   onClick={() => window.open(demoURL, '_blank', null)}
-                  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                  className={cardButton}
                   >Demo
                 </button>
                 <button 
                   onClick={() => window.open(codeURL, '_blank', null)}
-                  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                  className={cardButton}
                   >Code
                 </button>
               </div>

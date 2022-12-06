2fabf34f18f2a57da8013597f1cb74670ebb9f7d
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-06 15:00:04 -0600

    jsx cleanup contact component

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 
 const Contact = () => {
-  const init = { name: '', email: '', message: '' }
 
+  const init = { name: '', email: '', message: '' }
   const [form, setForm] = useState(init)
 
   const updateForm = (key, e) => {
@@ -30,19 +30,30 @@ const Contact = () => {
     return res;
   }
 
+  const styles = {
+    contactComponent: 'relative container mx-auto p-6 py-72 h-screen max-w-full bg-primary',
+    contactContainer: 'flex flex-col p-8 justify-center mx-auto h-full max-w-5xl',
+    header: 'text-primaryTxt mb-4 text-4xl text-center',
+    formContainer: 'flex justify-center items-center',
+    formStyles: 'flex flex-col w-full md:w-1/2',
+    formField: 'p-2 bg-transparent border-2 rounded-md text-secondaryTxt',
+    button: 'mt-2 p-3 px-6 pt-2 opacity-80 text-white bg-primaryTxt border-2 border-primaryTxt hover:opacity-100 duration-300 rounded-lg',
+  }
+  const {contactComponent, contactContainer, header, formContainer, formStyles, formField, button} = styles;
+
   return (
-    <div name='contact' className="relative container mx-auto p-6 py-72 h-screen max-w-full bg-primary">
-      <div className="flex flex-col p-8 justify-center mx-auto h-full max-w-5xl">
-        <h1 className="text-primaryTxt mb-4 text-4xl text-center">Contact</h1>
-        <div className="flex justify-center items-center">
-          <form onSubmit={submitForm} action="" className="flex flex-col w-full md:w-1/2">
+    <div name='contact' className={contactComponent}>
+      <div className={contactContainer}>
+        <h1 className={header}>Contact</h1>
+        <div className={formContainer}>
+          <form onSubmit={submitForm} action="" className={formStyles}>
             <input
               onChange={(e) => updateForm("name", e)} 
               type="text"
               name="name"
               placeholder="Enter your name"
               value={form.name}
-              className="p-2 bg-transparent border-2 rounded-md text-secondaryTxt"
+              className={formField}
             />
             <input
               onChange={(e) => updateForm("email", e)}
@@ -50,7 +61,7 @@ const Contact = () => {
               name="email"
               placeholder="Enter your email"
               value={form.email}
-              className="my-2 p-2 bg-transparent border-2 rounded-md text-secondaryTxt focus:outline-none"
+              className={`${formField} my-2 focus:outline-none`}
             />
             <textarea
               onChange={(e) => updateForm("message", e)}
@@ -59,10 +70,10 @@ const Contact = () => {
               placeholder="Enter your message"
               value={form.message}
               rows="10"
-              className="p-2 bg-transparent border-2 rounded-md text-secondaryTxt focus:outline-none">
+              className={`${formField} focus:outline-none`}>
             </textarea>
             <button
-              className="mt-2 p-3 px-6 pt-2 opacity-80 text-white bg-primaryTxt border-2 border-primaryTxt hover:opacity-100 duration-300 rounded-lg"
+              className={button}
               >Submit
             </button>
           </form>
